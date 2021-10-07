8fdc17684e87e59deb7224fd82710d4b8100ba05
jungmin kim <88076945+naturalMin@users.noreply.github.com>
2021-10-07 22:46:28 +0900

    Create start_over.py

---
+++ Python_Webscrapper_nomad/start_over.py
@@ -0,0 +1,22 @@
+import os
+
+#시작-종료 구현
+def start_over():
+  q = input("Do you want to restart? y or n ")
+  if q == "y":
+    os.system('clear')
+    get_bts()
+  elif q == "n":
+    print("Ok, bye")
+  else:
+    print("Incorrect Answer. choose y or n")  
+    start_over()
+
+def get_bts():
+  
+  print("Enter the member's name in BTS")
+  bts = input("").replace(" ","").split(",") 
+  for member in bts:
+    print(member)
+  start_over()
+get_bts()
