515e669375af1a500143725562d92c1bbed26e49
jungmin kim <88076945+naturalMin@users.noreply.github.com>
2021-10-12 16:21:22 +0900

    Update alba.py

--- Python_Webscrapper_nomad/alba.py
+++ Python_Webscrapper_nomad/alba.py
@@ -5,16 +5,32 @@ from bs4 import BeautifulSoup
 
 os.system("clear")
 
-jobs = []
+def save_to_file(name):  
+  name = company_list['company'].replace("/","")     
+  file = open(f"{name}.csv", mode="w")
+  writer = csv.writer(file)
+  writer.writerow(['place','title','time','pay','date'])  
+  for job in company_list['jobs']:
+    writer.writerow(list(job.values()))    
+  print(f"Successful Completed...{company_list['company']}")
 
 alba_url = "http://www.alba.co.kr"
 all_result = requests.get(alba_url)
 all_soup = BeautifulSoup(all_result.text, "html.parser")
 superbrand = all_soup.find("div",{"id" : "MainSuperBrand"})
-s_list = superbrand.find_all("a",{"class":"goodsBox-info"})
+s_list = superbrand.find_all("a",{"class":"goodsBox-info"})    
+        
+for s in s_list:     
+  link = s['href']
+  s_item = s.find("span",{"class":"company"})    
+  company = s_item.text    
+  if link and company:
+    company_list = {
+      'company':company, 
+      'jobs':[]
+      }
 
-def get_jobs(job_url):  
-  job_result = requests.get(job_url)
+  job_result = requests.get(link)
   job_soup = BeautifulSoup(job_result.text, "html.parser")
   job_table = job_soup.find("table")
   rows = job_table.find_all("tr")[1:]  
@@ -33,31 +49,5 @@ def get_jobs(job_url):
         "pay" : pay.text,
         "date" : regDate.text
       }
-      jobs.append(c_items)
-
-
-def save_to_file(name):  
-  name = name.replace("/","")     
-  file = open(f"{name}.csv", mode="w")
-  writer = csv.writer(file)
-  writer.writerow(['place','title','time','pay','date'])  
-  for job in jobs:
-    writer.writerow(list(job.values()))    
-  return             
-
-for s in s_list:
-  link = s['href']
-  s_item = s.find("span",{"class":"company"})    
-  company = s_item.text
-  if link and company:
-    s_items ={
-      'link' : link,
-      'company': company
-    }
-  get_jobs(s_items['link'])    
-  save_to_file(s_items['company'])
-
- 
-
-
-   
+      company_list['jobs'].append(c_items)
+  save_to_file(company_list['company'])   
