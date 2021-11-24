12ef566978b5d9924a24a168ac0ae801d9936b58
jungmin kim <88076945+naturalMin@users.noreply.github.com>
2021-11-24 17:23:19 +0900

    Add files via upload

---
+++ react-for-beginners/coin_tracker/App.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    })
+  },[])
+  return (       
+    <div>
+      <h1>Coin Tracker {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? <b>loading...</b> : null}
+      <ul>
+        {coins.map((coin) => <li key = {coin.id}>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}USD</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
